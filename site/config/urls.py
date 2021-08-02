edf773169b48d74c31ed11891a6ff11db53eda93
gghotted <gghotted2@naver.com>
2021-08-02 12:45:42 +0900

    Update app url

--- site/config/urls.py
+++ site/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('moviemon/', include('moviemon.urls'))
+    path('', include('moviemon.urls'))
 ]
