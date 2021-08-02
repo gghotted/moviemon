e01c20988e7d7ef245c4befcb91001b5662c9f90
gghotted <gghotted2@naver.com>
2021-08-02 12:56:04 +0900

    Add app urls

--- site/moviemon/urls.py
+++ site/moviemon/urls.py
@@ -3,5 +3,12 @@ from .views import TitleScreen
 
 
 urlpatterns = [
-    path('', TitleScreen.as_view(), name='title_screen')
+    path('', TitleScreen.as_view(), name='title_screen'),
+    # path('worldmap', [...], name='worldmap'),
+    # path('battle/<int:moviemon_id>', [...], name='battle'),
+    # path('moviedex', [...], name='moviedex'),
+    # path('moviedex/<int:moviemon_id>', [...], name='moviedex_detail'),
+    # path('options', [...], name='options'),
+    # path('options/save_game', [...], name='save'),
+    # path('options/load_game', [...], name='load'),
 ]
