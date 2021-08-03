6f824df40374b5b34bd7c64be1bbb3da07acbaac
gghotted <gghotted2@naver.com>
2021-08-03 15:30:07 +0900

    End

--- site/moviemon/urls.py
+++ site/moviemon/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('worldmap', WorldMap.as_view(), name='worldmap'),
     path('battle/<str:moviemon_id>', Battle.as_view(), name='battle'),
     path('moviedex', Moviedex.as_view(), name='moviedex'),
-    path('moviedex/<int:moviemon_id>', MoviedexDetail.as_view(), name='moviedex_detail'),
+    path('moviedex/<str:moviemon_id>', MoviedexDetail.as_view(), name='moviedex_detail'),
     path('options', Options.as_view(), name='options'),
     path('options/save_game', Save.as_view(), name='save'),
     path('options/load_game', Load.as_view(), name='load'),
+    path('win', Win.as_view(), name='win')
 ]
--- site/moviemon/views.py
+++ site/moviemon/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpRequest
 from django.conf import settings
 import random
 import pickle
+import os
 
 class TitleScreen(TemplateView):
     template_name = 'moviemon/title_screen.html'
@@ -50,19 +51,23 @@ class WorldMap(TemplateView):
             gamedata.pos['x'] += 1
 #        print('cmd: ', request.GET.get('cmd'))
 #        print('x: ', gamedata.pos['x'])
+        if len(gamedata.moviemons) == len(gamedata.movie_get):
+            return redirect('win')
 
-        eventlist = ['ball', 'non', 'non']
-        if len(gamedata.moviemons) != len(gamedata.movie_get):
-            eventlist.append('moviemon')
-        event = random.choice(eventlist)
-        if event == 'non':
-            self.movieball_appeared = False
-            self.moviemon_appeared = False
-        elif event == 'ball':
-            self.movieball_appeared = True
-            gamedata.movie_ball_count += 1
-        elif event == 'moviemon':
-            self.moviemon_appeared = True
+        if request.GET.get('cmd'):
+            eventlist = ['ball', 'non', 'non']
+            if len(gamedata.moviemons) != len(gamedata.movie_get):
+                eventlist.append('moviemon')
+
+            event = random.choice(eventlist)
+            if event == 'non':
+                self.movieball_appeared = False
+                self.moviemon_appeared = False
+            elif event == 'ball':
+                self.movieball_appeared = True
+                gamedata.movie_ball_count += 1
+            elif event == 'moviemon':
+                self.moviemon_appeared = True
 
         # self.gamedata.save(request.session['filepath'])
         request.session['gamedata'] = pickle.dumps(gamedata).hex()
@@ -123,7 +128,6 @@ class Battle(TemplateView):
     moviemon_strength = float()
     missed = False
     catched = False
-
 
     def get(self, request, *args, **kwargs):
         self.moviemon_id = kwargs['moviemon_id']
@@ -188,22 +192,225 @@ class Battle(TemplateView):
 
 class Moviedex(TemplateView):
     template_name = 'moviemon/moviedex.html'
+    moviemons = []
+    selected_idx = 0
+    selected_id = 'None'
+    moviemons_cnt = 0
 
     def get(self, request, *args, **kwargs):
+        gamedata = Data.get_user_game_data(self.request, newgame=False)
+        self.selected_idx = int(request.GET.get('selected_idx', 0))
+        self.moviemons = []
+        for title in gamedata.movies_detail:
+            if title in gamedata.movie_get:
+                moviemon = gamedata.movies_detail[title]
+                self.moviemons.append(moviemon)
+        self.moviemons_cnt = len(self.moviemons)
+
+        if request.GET.get('cmd') == 'left' and self.selected_idx > 0:
+            self.selected_idx -= 1
+        if request.GET.get('cmd') == 'right' and self.selected_idx < self.moviemons_cnt - 1:
+            self.selected_idx += 1
+        if self.moviemons_cnt > 0:
+            self.selected_id = self.moviemons[self.selected_idx]['imdbID']
 
         return super().get(request, *args, **kwargs)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['btn_enable'] = {
+            'left': True,
+            'right': self.moviemons_cnt != 0,
+            'up': False,
+            'down': False,
+            'select': True,
+            'start': False,
+            'a': self.selected_id != 'None',
+            'b': False
+        }
+        context['selected_id'] = self.selected_id
+        context['moviemons'] = self.moviemons
+        context['selected_idx'] = self.selected_idx
+        if self.selected_idx == 0:
+            context['btn_enable']['left'] = False
+        if self.selected_idx == self.moviemons_cnt - 1:
+            context['btn_enable']['right'] = False
+        return context
+
+
 class MoviedexDetail(TemplateView):
     template_name = 'moviemon/moviedex_detail.html'
+    moviemon_id = ''
+
+    def get(self, request, *args, **kwargs):
+        self.moviemon_id = kwargs['moviemon_id']
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        gamedata = Data.get_user_game_data(self.request, newgame=False)
+        context.update({
+            'btn_enable': {
+                'b': True
+            }
+        })
+        for movie in gamedata.movies_detail:
+            if gamedata.movies_detail[movie]['imdbID'] == self.moviemon_id:
+                context['moviemon'] = gamedata.movies_detail[movie]
+        return context
 
 
 class Options(TemplateView):
     template_name = 'moviemon/options.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'btn_enable': {
+                'start': True,
+                'a': True,
+                'b': True
+            }
+        })
+        return context
+
 
 class Save(TemplateView):
     template_name = 'moviemon/save.html'
+    slot_idx = 'a'
+
+    def get(self, request, *args, **kwargs):
+        gamedata = Data.get_user_game_data(self.request, newgame=False)
+        self.slot_idx = request.GET.get('slot_idx', 'a')
+        cmd = request.GET.get('cmd')
+
+        if cmd == 'up' and self.slot_idx != 'a':
+            self.slot_idx = chr(ord(self.slot_idx) - 1)
+        elif cmd == 'down' and self.slot_idx != 'c':
+            self.slot_idx = chr(ord(self.slot_idx) + 1)
+        elif cmd == 'a':
+            self.save_gamedata(gamedata)
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'btn_enable': {
+                'up': True,
+                'down': True,
+                'a': True,
+                'b': True
+            }
+        })
+        context['slots_state'] = {
+            'a': self.parse_slot_score('a'),
+            'b': self.parse_slot_score('b'),
+            'c': self.parse_slot_score('c')
+        }
+        context['slot_idx'] = self.slot_idx
+        if self.slot_idx == 'a': context['btn_enable']['up'] = False
+        if self.slot_idx == 'c': context['btn_enable']['down'] = False
+        return context
+
+    def parse_slot_score(self, slot_idx):
+        filename = self.get_filename(slot_idx)
+        if not filename:
+            return 'free'
+        else:
+            return filename[6:-4].replace('_', '/')
+
+    def save_gamedata(self, data):
+        filename = self.get_filename(self.slot_idx)
+        if filename:
+            os.remove(f'{settings.BASE_DIR}/moviemon/saved_game/{filename}')
+
+        score = f'{len(data.movie_get)}_{len(data.moviemons)}'
+
+        path = f'{settings.BASE_DIR}/moviemon/saved_game/slot{self.slot_idx}_{score}.mmg'
+        with open(path, 'wb') as file:
+            pickle.dump(data, file)
+
+    def get_filename(self, slot_idx):
+        root = f'{settings.BASE_DIR}/moviemon/saved_game'
+        for file in os.listdir(root):
+            if file.startswith(f'slot{slot_idx}'):
+                return file
+        return None
 
 
 class Load(TemplateView):
     template_name = 'moviemon/load.html'
+    slot_idx = 'a'
+    loaded = False
+
+    def get(self, request, *args, **kwargs):
+        self.slot_idx = request.GET.get('slot_idx', 'a')
+        cmd = request.GET.get('cmd')
+
+        if cmd == 'up' and self.slot_idx != 'a':
+            self.slot_idx = chr(ord(self.slot_idx) - 1)
+        elif cmd == 'down' and self.slot_idx != 'c':
+            self.slot_idx = chr(ord(self.slot_idx) + 1)
+        elif cmd == 'a' and self.parse_slot_score(self.slot_idx) != 'free':
+            self.load_gamedata()
+            self.loaded = True
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'btn_enable': {
+                'up': True,
+                'down': True,
+                'a': True,
+                'b': True
+            }
+        })
+        context['slots_state'] = {
+            'a': self.parse_slot_score('a'),
+            'b': self.parse_slot_score('b'),
+            'c': self.parse_slot_score('c')
+        }
+        context['slot_idx'] = self.slot_idx
+        context['loaded'] = self.loaded
+        if context['slots_state'][self.slot_idx] == 'free':
+            context['btn_enable']['a'] = False
+        if self.slot_idx == 'a': context['btn_enable']['up'] = False
+        if self.slot_idx == 'c': context['btn_enable']['down'] = False
+        return context
+
+    def parse_slot_score(self, slot_idx):
+        filename = self.get_filename(slot_idx)
+        if not filename:
+            return 'free'
+        else:
+            return filename[6:-4].replace('_', '/')
+
+    def load_gamedata(self):
+        path = f'{settings.BASE_DIR}/moviemon/saved_game/{self.get_filename(self.slot_idx)}'
+        with open(path, 'rb') as file:
+            self.request.session['gamedata'] = file.read().hex()
+
+    def get_filename(self, slot_idx):
+        root = f'{settings.BASE_DIR}/moviemon/saved_game'
+        for file in os.listdir(root):
+            if file.startswith(f'slot{slot_idx}'):
+                return file
+        return None
+
+class Win(TemplateView):
+    template_name = 'moviemon/win.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'btn_enable': {
+                'a': True,
+                'b': True
+            }
+        })
+        return context
